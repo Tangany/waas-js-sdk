45aec183ac106315198a9bae9d158b3a5ec54a8a
Alexey Utin <alexey.utin@tangany.com>
2020-01-07 10:48:30 +0100

    Fetch the actual api version from the oas definition

--- scripts/get-sdk-version-badge.js
+++ scripts/get-sdk-version-badge.js
@@ -1,7 +1,17 @@
 const
 	fileName = "sdk-badge.svg"
 	, createBadge = require("./badge")
+	, axios = require("axios")
 ;
+(async function () {
+	const { data: oas } = await axios.get("https://tangany.docs.stoplight.io/api/api.tangany.com.oas2.json");
+	const version = oas.info.version;
+	if (!version) {
+		console.error(oas);
+		throw new Error("Couldn't parse version for oas definition");
+	}
 
-// downloads a version badge to destination directory after `npm version` for current branches master and development
-createBadge("WaaS API", "1.2", fileName, "orange");
+	// downloads a version badge to destination directory after `npm version` for current branches master and development
+	createBadge("WaaS API", version, fileName, "orange");
+})();
+
