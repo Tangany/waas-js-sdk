98328685f4a3cf6dacc1ac70c1aa9ed487d5b23e
Adrian Mitter <adrian.mitter@tangany.com>
2020-07-16 16:40:29 +0200

    Add activity id to thrown errors

--- src/errors/conflict-error.ts
+++ src/errors/conflict-error.ts
@@ -1,8 +1,8 @@
 import {HttpError} from "./http-error";
 
 export class ConflictError extends HttpError {
-    constructor(msg = "Cannot reset resource") {
-        super(msg, 409);
+    constructor(msg = "Cannot reset resource", activityId: string) {
+        super(msg, 409, activityId);
         Error.captureStackTrace(this, this.constructor);
         this.name = this.constructor.name;
         Object.setPrototypeOf(this, ConflictError.prototype);
